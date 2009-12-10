f6523784256b88b51dbc0b1e029404650a1d6da7
Mark Grimes <mgrimes@peculier.com>
2009-12-10 09:57:48 -0500

    Added a sample script

---
+++ samples/simple.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use lib qw(lib ../lib);
+
+use AnyEvent::Filesys::Notify;
+use AnyEvent::Impl::Perl;  # Could be use Event or use EV, see AnyEvent
+
+my $notifier = AnyEvent::Filesys::Notify->new(
+    dirs     => [qw(lib t)],
+    interval => 0.5,
+    cb       => sub {
+        my @events = @_;
+
+        printf "%s %s\n", $_->path, $_->type for @events;
+        ## Do something with them...
+    },
+);
+
+print "Watching " . join( ", ", @{ $notifier->dirs } ) . " for changes.\n";
+AnyEvent::Impl::Perl::loop();
