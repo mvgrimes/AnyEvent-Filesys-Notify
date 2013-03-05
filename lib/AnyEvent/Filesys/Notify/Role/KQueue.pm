3fe60f00b7de9c727cd297a7ee681db6b35c0a77
Mark Grimes <mgrimes@cpan.org>
2013-03-05 13:12:55 -0500

    Fixes KQueue backend bug which causes CPU usage to peg at 100%
    - Adds EV_CLEAR to KQueue to avoid firing multiple times for each event
      (thanks to gdxhsw++ for the bug report and patch)
      https://github.com/gdxhsw/AnyEvent-Filesys-Notify
    
    - Adds each subdirectory and file to the kqueue watch. This is not
      practical as you need to open a filehandle for each monitored path.

--- lib/AnyEvent/Filesys/Notify/Role/KQueue.pm
+++ lib/AnyEvent/Filesys/Notify/Role/KQueue.pm
@@ -7,38 +7,71 @@ use namespace::autoclean;
 use AnyEvent;
 use IO::KQueue;
 use Carp;
+use Try::Tiny;
 
 sub _init {
     my $self = shift;
 
     my $kqueue = IO::KQueue->new()
       or croak "Unable to create new IO::KQueue object";
+    $self->_fs_monitor($kqueue);
 
-    my @fhs = map {
-        open my $fh, '<', $_ or croak "Can't open file: $_";
+    # Need to add all the subdirs to the watch list, this will catch
+    # modifications to files too.
+    my $old_fs = $self->_old_fs;
+    my @paths  = keys %$old_fs;
 
-        $kqueue->EV_SET(
-            fileno($fh),
-            EVFILT_VNODE,
-            EV_ADD | EV_ENABLE,
-            NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB |
-            NOTE_LINK | NOTE_RENAME | NOTE_REVOKE,
-        );
-        $fh;
-    } @{ $self->dirs };
+    # Add each file and each directory
+    my @fhs;
+    for my $path (@paths) {
+        push @fhs, $self->_watch($path);
+    }
 
+    # Now use AE to watch the KQueue
     my $w;
     $w = AE::io $$kqueue, 0, sub {
+        warn "# event\n";
         if ( my @events = $kqueue->kevent ) {
             $self->_process_events(@events);
         }
     };
-
-    $self->_fs_monitor($kqueue);
-
     $self->_watcher( { fhs => \@fhs, w => $w } );
 
     return 1;
+}
+
+# Need to add newly created items (directories and files).
+# This is done after filtering. So entire dirs can be ignored efficiently.
+around '_process_events' => sub {
+    my ( $orig, $self, @e ) = @_;
+
+    my $events = $self->$orig(@e);
+
+    for my $event (@$events) {
+        next unless $event->is_created;
+
+        my $fh = $self->_watch( $event->path );
+        push @{ $self->_watcher->{fhs} }, $fh;
+
+    }
+
+    return $events;
+};
+
+sub _watch {
+    my ( $self, $path ) = @_;
+
+    open my $fh, '<', $path or croak "Can't open file ($path): $!";
+
+    $self->_fs_monitor->EV_SET(
+        fileno($fh),
+        EVFILT_VNODE,
+        EV_ADD | EV_ENABLE | EV_CLEAR,
+        NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK |
+          NOTE_RENAME | NOTE_REVOKE,
+    );
+
+    return $fh;
 }
 
 1;
